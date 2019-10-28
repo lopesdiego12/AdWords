816630951c3661fa096c94564d416b4d92ed7cb3
Diego Lopes <lopesdiego12@gmail.com>
2019-10-28 14:49:57 -0300

    adding

---
+++ accounts_config.py
@@ -0,0 +1,10 @@
+# arquivo com o número das contas do google ads que não estao vinculadas com a MCC
+
+def get_accounts():
+	accounts = type("", (object,), {})()	
+	# id das contas
+	accounts.account_id = [
+					# Captação
+					'*******']
+	
+	return accounts;
